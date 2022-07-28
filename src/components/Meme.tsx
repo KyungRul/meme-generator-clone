44561ed677212adab7b09305e23d39dad9f2a90a
kyungrulbae <bjoh100@gmail.com>
2022-07-28 14:25:54 +0900

    Applied useState

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,21 +1,62 @@
 import React from 'react'
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+  const [allMemeImage, setAllMemeImage] = React.useState(memesData)
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: memesArray[randomNumber].url
+    }))
+  }
+
+  function handleChange(event: { target: { name: any, value: any } }) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
+  }
+
+
   return (
     <main>
-      <form>
+      <div className="form">
         <input
           type={"text"}
           placeholder={"Top text"}
-          className={"form--input"}/>
+          className={"form--input"}
+          name={"topText"}
+          value={meme.topText}
+          onChange={handleChange}
+        />
         <input
           type={"text"}
           placeholder={"Bottom text"}
-          className={"form--input"}/>
+          className={"form--input"}
+          name={"bottomText"}
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button
-          className={"form--button"}>Get a new meme image
+          className={"form--button"}
+          onClick={getMemeImage}
+        >Get a new meme image
         </button>
-      </form>
+      </div>
+      <div className={"meme"}>
+        <img src={meme.randomImage} className={"meme-image"}/>
+        <h2 className={"meme--text top"}>{meme.topText}</h2>
+        <h2 className={"meme--text bottom"}>{meme.bottomText}</h2>
+      </div>
     </main>
   )
 }
